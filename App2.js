0aa250b045e892dd115aada66d3869ebb15b743f
Ronald Benitez <ronald05101998@gmail.com>
2022-04-26 23:16:29 -0500

    FlatList

--- App2.js
+++ App2.js
@@ -15,6 +15,7 @@ import {
   Button,
   ScrollView,
   RefreshControl,
+  FlatList,
 
 } from 'react-native';
 
@@ -30,16 +31,18 @@ import {
 const App = () => {
 
   const [Items,setItems] = useState([
-    {key:1,item:'Item 1'},
-    {key:1,item:'Item 2'},
-    {key:1,item:'Item 3'},
-    {key:1,item:'Item 4'},
-    {key:1,item:'Item 5'},
-    {key:1,item:'Item 6'},
-    {key:1,item:'Item 7'},
-    {key:1,item:'Item 8'},
-    {key:1,item:'Item 9'},
+    {name:'Item 1'},
+    {name:'Item 2'},
+    {name:'Item 3'},
+    {name:'Item 4'},
+    {name:'Item 5'},
+    {name:'Item 6'},
+    {name:'Item 7'},  
+    {name:'Item 8'},
+    {name:'Item 9'},
+    {name:'Item 10'},
   ]);
+  
 
   const [refreshing,setRefreshing] = useState(t=false);
   const onRefresh = () => {
@@ -49,29 +52,38 @@ const App = () => {
     }
 
   return (
-      <ScrollView 
-      style={styles.body}
-      refreshControl={
-        <RefreshControl
-        refreshing={refreshing}
-        onRefresh={onRefresh}     
-        colors={['blue']}
-        />
-      }      
-      >
+    <FlatList
+      data={Items}
+      keyExtractor={(item,index)=>index.toString()}
+      renderItem={({item}) => (
+        <View style={styles.item}>
+          <Text style={styles.text}>{item.name}</Text>
+        </View>
+      )}
+    />
+      // <ScrollView 
+      // style={styles.body}
+      // refreshControl={
+      //   <RefreshControl
+      //   refreshing={refreshing}
+      //   onRefresh={onRefresh}     
+      //   colors={['blue']}
+      //   />
+      // }      
+      // >
 
-      {
-        Items.map((item)=>{
-          return(
+      // {
+      //   Items.map((item)=>{
+      //     return(
            
-          <View style={styles.item} key={item.key}>
-            <Text style={styles.text}> {item.item + console.log(item.key)}</Text>
-          </View>
-          )
-        })
-      }
+      //     <View style={styles.item} key={item.key}>
+      //       <Text style={styles.text}> {item.item + console.log(item.key)}</Text>
+      //     </View>
+      //     )
+      //   })
+      // }
     
-      </ScrollView>
+      // </ScrollView>
   );
 };
 
